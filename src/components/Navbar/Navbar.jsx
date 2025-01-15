dca85d650b9ec77467d62270c22e2df99bfca5b5
Herryking25 <bolatitoheritage25@gmail.com>
2025-01-14 21:10:34 -0900

    Almost done with Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import {IoMdSearch} from 'react-icons/io'
+import { FaCartShopping } from "react-icons/fa6";
+import Darkmode from '../Darkmode';
 
 const MenuLinks = [
   {
@@ -27,9 +30,9 @@ const Navbar = () => {
   return (
     <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40'>
       <div className='py-4'>
-        <div className="container">
+        <div className="container flex justify-between items-center">
             {/* Logo and Links Section */}
-                <div className='flex items-center gap-8'>
+                <div className='flex items-center gap-4'>
                     <a href className='text-primary font-semibold tracking-widest 
                     text-2xl sm:text-3xl'>
                         King-Shop
@@ -52,14 +55,27 @@ const Navbar = () => {
                 </div>
 
             {/* Navbar Right Section */}
-              <div>
+              <div className='flex justify-between items-center gap-4'>
                 {/* Search Bar Section */}
-                <div>
-                    <input type="text" placeholder='Search' className='' />
-                    <IoMdSearch/>
+                <div className='relative group hidden sm:block'>
+                    <input type="text" placeholder='Search' className='search-bar' />
+                    <IoMdSearch  className='text-xl text-gray-600 group-hover:text-primary dark:text-gray-400 absolute
+                    top-1/4 -translate-y-1/2: right-3 duration-200' />
                 </div>
+
+                {/* Order Section */}
+                        <button className='relative p-3'>
+                        <FaCartShopping className='text-xl text-gray-600 dark:text-gray-400' />
+                        <div className='w-4 h-4 bg-red-500 text-white rounded-full absolute top-0 right-0 
+                        flex justify-center items-center text-xs'>
+                          4
+                        </div>
+                        </button>
+
                 {/* Dark mode Section */}
-                <div></div>
+                <div>
+                  <Darkmode />
+                </div>
               </div>
         </div>
       </div>
